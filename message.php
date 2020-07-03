05bdedb7fc7682cb557f909af9c8d1d4558aaf48
NezhaElKihel <60564553+Nezha-El-Kihel@users.noreply.github.com>
2020-07-03 16:52:48 +0100

    Add files via upload

---
+++ message.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Memory Gard Game</title>
+	<link rel="stylesheet" type="text/css" href="memory_card_game.css">
+	<meta charset="utf-8">
+	<meta http-equiv= x-ua-compatible content= ie=edge>
+	<meta name= viewport content= width=device-width initial-scale=1>
+	<link rel= stylesheet href= "https //maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src= "https //ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src= "https //maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<style>
+		*{
+			margin: 0;
+			padding: 0;
+			box-sizing: border-box;
+			user-select: none;
+			text-decoration: none;
+			color: white;
+		}
+		body{
+			height: 100vh;
+			justify-content: center;
+			align-items: center;
+			text-align: center;
+			background-color: black;
+
+		}
+		#message_box{
+			width: 600px;
+			height: 400px;
+			border-radius: 10px;
+			padding: 70px 50px;
+		}
+		p{
+			margin-top: 30px;
+			font-family: 'Gloria Hallelujah', cursive;
+		}
+		.fa{
+			margin-top: 40px;
+			font-size: 1.5em;
+		}
+		a{
+			padding: 20px;
+		}
+		a:hover, a:focus{
+			cursor: pointer;
+			color: purple;
+			background-color: purple;
+		}
+	</style>
+</head>
+<body>
+	<div id="message_box">
+		<p>-- If you have any comments or suggestions that would help us improve this guide, we would like to hear from you. --</p>
+		<p>-- Si vous avez des commentaires ou des suggestions qui nous aideraient à améliorer ce mini-projet, n'hésitez pas à nous les transmettre. --</p>
+		<a href="https://www.instagram.com/crochetdfamily/"><i class="fa fa-instagram"></i> <span>Follow me</span></a>
+	</div>
+</body>
+</html>
